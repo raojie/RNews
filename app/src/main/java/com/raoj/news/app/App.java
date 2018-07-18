1632660f36529664cf60905354876e48c650d9fa
raoj <156321693@qq.com>
2018-07-18 18:23:36 +0800

    添加全局application类， 并添加初始化日志打印模块， 添加初始化内存泄漏监测模块。

---
+++ app/src/main/java/com/raoj/news/app/App.java
@@ -0,0 +1,40 @@
+package com.raoj.news.app;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.socks.library.KLog;
+import com.squareup.leakcanary.BuildConfig;
+import com.squareup.leakcanary.LeakCanary;
+import com.squareup.leakcanary.RefWatcher;
+
+/**
+ * 
+ * @class App
+ * @describe describe
+ * @author raoj
+ * @date 2018-07-18
+ *
+ */
+public class App extends Application {
+
+    private RefWatcher mRefWatcher;
+
+    private static Context sApplicationContext;
+
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // 如果检测到某个 activity 有内存泄露，LeakCanary 就是自动地显示一个通知
+        mRefWatcher = LeakCanary.install(this);
+
+        sApplicationContext = this;
+
+        //日志工具初始化
+        KLog.init(BuildConfig.DEBUG);
+    }
+    
+}
