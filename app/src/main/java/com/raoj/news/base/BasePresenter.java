2acc8e1a8b17f21b9b394483d72eac0b7c3b80c9
raoj <156321693@qq.com>
2018-08-31 15:49:46 +0800

    添加代理基类

---
+++ app/src/main/java/com/raoj/news/base/BasePresenter.java
@@ -0,0 +1,15 @@
+package com.raoj.news.base;
+
+/**
+ * class BasePresenter
+ * describe 代理的基类
+ * author raoj
+ * date 2018-08-31
+ */
+public interface BasePresenter {
+
+    void onResume();
+
+    void onDestroy();
+
+}
