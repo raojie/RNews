bc9d7072a0d0b79d253b7a6a7a464dd274dcdc80
raoj <156321693@qq.com>
2018-08-31 15:46:09 +0800

    添加视图基类

---
+++ app/src/main/java/com/raoj/news/base/BaseView.java
@@ -0,0 +1,17 @@
+package com.raoj.news.base;
+
+/**
+ * class BaseView
+ * describe 视图基类
+ * author raoj
+ * date 2018-08-31
+ */
+public interface BaseView {
+
+    void toast(String msg);
+
+    void showProgress();
+
+    void hideProgress();
+
+}
